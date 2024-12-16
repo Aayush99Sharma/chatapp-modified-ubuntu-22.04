03410868a7e5d986f31e44b8e1783a4c12e0083c
Aayush Sharma <ayyush00sharma@gmail.com>
2024-12-16 05:10:13 +0000

    first

--- fundoo/chat/urls.py
+++ fundoo/chat/urls.py
@@ -1,10 +1,8 @@
-# chat/urls.py
-from django.conf.urls import url
 from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.chatting, name='chat'),
-    re_path(r'(?P<room_name>[^/]+)/$', views.room, name='room'),
+    re_path(r'^$', views.chatting, name='chat'),  # Home page for chat
+    re_path(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),  # Specific room
 ]
--- fundoo/fundoo/urls.py
+++ fundoo/fundoo/urls.py
@@ -1,24 +1,31 @@
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from fundooapp import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # Admin Panel
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('signup/', views.Signup, name='signup'),  # Registration using REST.
-    path('sign_in/', views.login_u, name='sign_in'),       # REST Login.
-    url(r'^login/$', views.user_login, name='login'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate'),
-    path('chat/', include('chat.urls')),
-    path('chat/', include('django.contrib.auth.urls')),
-    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    url(r'^logout/$', views.index, name='logout')
 
+    # Home page and sign-up views
+    path('', views.index, name='index'),
+    path('signup/', views.Signup, name='signup'),
+    path('sign_in/', views.login_u, name='sign_in'),
+
+    # Account activation URL (use re_path for regex)
+    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+            views.activate, name='activate'),
+
+    # Chat app URLs
+    path('chat/', include('chat.urls')),
+
+    # Django Auth URLs (includes login, logout, password reset)
+    path('accounts/', include('django.contrib.auth.urls')),  # This will include login, logout, password reset etc.
+
+    # Password reset views (if needed explicitly)
+    re_path(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    re_path(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+            auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    re_path(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
